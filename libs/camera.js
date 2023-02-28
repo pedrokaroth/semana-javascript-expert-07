65f65129ea6447d5e8224f3d7e3fe8a10be0ea07
pedrokaroth <pedrokaroth@gmail.com>
2023-02-28 19:59:20 -0300

    chore: acessos e elemento de video

---
+++ libs/camera.js
@@ -0,0 +1,37 @@
+export default class Camera {
+  constructor () {
+    this.video = document.createElement('video')
+  }
+
+  static async init () {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw new Error('Browser API navigator.mediaDevices.getUserMedia is not available')
+    }
+
+    const camera = new Camera()
+
+    camera.video.srcObject = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        width: globalThis.screen.availWidth,
+        height: globalThis.screen.availHeight,
+        frameRate: { ideal: 60 }
+      }
+    })
+
+    camera.video.height = 240
+    camera.video.width = 320
+
+    document.body.append(camera.video)
+
+    await new Promise((resolve) => {
+      camera.video.onloadedmetadata = () => {
+        resolve(camera.video)
+      }
+    })
+
+    camera.video.play()
+
+    return camera
+  }
+}
--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -1,8 +1,11 @@
+import Camera from '../../../libs/camera.js'
 import Controller from './controller.js'
 import Service from './service.js'
 import View from './view.js'
 
 const [path] = window.location.href.split('/pages')
+
+await Camera.init()
 
 console.log(path)
 
